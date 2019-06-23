2749d267cb2242404ad291a23867d0824e8ad96d
mpivet-p <mpivet-p@student.42.fr>
2019-06-23 08:50:44 +0200

    parsing done

--- src/main.c
+++ src/main.c
@@ -6,13 +6,13 @@
 /*   By: mpivet-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/22 00:26:11 by mpivet-p          #+#    #+#             */
-/*   Updated: 2019/06/23 04:06:24 by mpivet-p         ###   ########.fr       */
+/*   Updated: 2019/06/23 08:31:52 by mpivet-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	fdf_draw_line(t_fmlx *ptr, t_point one, t_point two)
+void	fdf_draw_line(t_fmlx *ptr, t_point one, t_point two, int color)
 {
 	t_point	delta;
 	t_point	s;
@@ -32,7 +32,7 @@ void	fdf_draw_line(t_fmlx *ptr, t_point one, t_point two)
 		while (one.x != two.x)
 		{
 			mlx_pixel_put(ptr->mlx, ptr->win, one.x,
-					(int)lround((slope * (double)one.x) + pitch), 0xFF0000);
+					(int)lround((slope * (double)one.x) + pitch), color);
 			one.x += s.x;
 		}
 		return ;
@@ -41,8 +41,8 @@ void	fdf_draw_line(t_fmlx *ptr, t_point one, t_point two)
 	pitch = (double)one.x - (slope * (double)one.y);
 	while (one.y != two.y)
 	{
-		mlx_pixel_put(ptr->mlx, ptr->win, (int)lround((slope * (double)one.y) + pitch),
-				one.y, 0xFF0000);
+		mlx_pixel_put(ptr->mlx, ptr->win,
+				(int)lround((slope * (double)one.y) + pitch), one.y, color);
 		one.y += s.y;
 	}
 }
@@ -54,6 +54,50 @@ t_point	fdf_point(int x, int y)
 	ret.x = x;
 	ret.y = y;
 	return (ret);
+}
+
+t_point	fdf_coords(int x, int y)
+{
+	t_point ret;
+
+	ret.x = 200 + (x * 20);
+	ret.y = 200 + (y * 20);
+	return (ret);
+}
+
+void	disp_connect_points(t_fmlx *mlx, t_fmap *ptr)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (i < ptr->size_y)
+	{
+		while (j < ptr->size_x)
+		{
+			if (i + 1 < ptr->size_y)
+				fdf_draw_line(mlx, fdf_coords(j, i), fdf_coords(j, i + 1), 0xFFFFFF);
+			if (i + 1 < ptr->size_y && ptr->map[i][j] > 0 && ptr->map[i + 1][j] > 0)
+				fdf_draw_line(mlx, fdf_coords(j, i), fdf_coords(j, i + 1), 0x4073ed);
+			if (j + 1 < ptr->size_x)
+				fdf_draw_line(mlx, fdf_coords(j, i), fdf_coords(j + 1, i), 0xFFFFFF);
+			if (j + 1 < ptr->size_x && ptr->map[i][j] > 0 && ptr->map[i][j + 1] > 0)
+				fdf_draw_line(mlx, fdf_coords(j, i), fdf_coords(j + 1, i), 0x4073ed);
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+}
+
+t_point		test_matrice(t_point point, int angle)
+{
+	(void)angle;
+	point.x = (point.x * cos(angle)) + ((-1 * sin(angle)) * point.y);
+	point.y = (point.x * sin(angle)) + (cos(angle) * point.y);
+	printf("x = %i   y = %i\n", point.x, point.y);
+	return (point);
 }
 
 int		deal_key(int key, void *param)
@@ -73,8 +117,10 @@ void	fdf(t_fmap *map)
 		return ;
 	if (!(ptr.win = mlx_new_window(ptr.mlx, SWIN_X, SWIN_X, "test mlx")))
 		return ;
-	fdf_draw_line(&ptr, fdf_point(42, 42), fdf_point(430, 750));
 	mlx_key_hook(ptr.win, deal_key, (void *)0);
+	fdf_draw_line(&ptr, fdf_point(42, 42), fdf_point(150, 42), 0x0000FF);
+	fdf_draw_line(&ptr, test_matrice(fdf_point(42, 100), -30), test_matrice(fdf_point(150, 100), -30), 0xFFFFFF);
+	disp_connect_points(&ptr, map);
 	mlx_loop(ptr.mlx);
 
 }
@@ -85,7 +131,7 @@ int		main(int argc, char **argv)
 
 	(void)argc;
 	map.size_x = -1;
-	printf("\n%i\n", fdf_parse(argv[1], &map));
-//	fdf(&map);
+	fdf_parse(argv[1], &map);
+	fdf(&map);
 	return (0);
 }
--- src/parsing.c
+++ src/parsing.c
@@ -6,35 +6,13 @@
 /*   By: mpivet-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/22 02:45:38 by mpivet-p          #+#    #+#             */
-/*   Updated: 2019/06/23 03:19:48 by mpivet-p         ###   ########.fr       */
+/*   Updated: 2019/06/23 04:57:55 by mpivet-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int		parse_fillandcheck(const int fd, char ***tab)
-{
-	char	*line;
-	int		ret;
-	int		len;
-	int		i;
-
-	line = NULL;
-	i = 0;
-	len = -1;
-	ret = 0;
-	while ((ret = get_next_line(fd, &line)) > 0)
-	{
-		if ((tab[i] = ft_strsplit(line, ' ')) == NULL)
-			return (-1);
-		//if (parse_checkline(tab[i], len) != 0)
-		//	return (1);
-		i++;
-	}
-	return (ret);
-}
-
-int **delmap(int ***map)
+int **delmap(int ***map, int y)
 {
 	int		**ptr;
 	int		i;
@@ -45,11 +23,10 @@ int **delmap(int ***map)
 	{
 		ptr = *map;
 		*map = NULL;
-		while (ptr[i])
+		while (i < y)
 		{
 			free(ptr[i]);
 			ptr[i] = NULL;
-			i++;
 		}
 		free(ptr);
 	}
@@ -63,36 +40,82 @@ int		**gen_map(int x, int y)
 
 	i = 0;
 	map = NULL;
-	if (!(map = (int**)malloc(sizeof(int*) * y)))
+	if (!(map = (int**)malloc(sizeof(int*) * (y))))
 		return (NULL);
-	while (i != y)
+	while (i < y)
 	{
-		if (!(map[i] = (int*)malloc(sizeof(int) * x)))
-			return (delmap(&map));
-		if (i + 1 < y)
-			map[i + 1] = NULL;
+		if (!(map[i] = (int*)malloc(sizeof(int) * (x))))
+			return (delmap(&map, i));
 		i++;
 	}
 	return (map);
 }
 
+int		fill_map(char *path, t_fmap *map, char **tab)
+{
+	char	*line;
+	int		x;
+	int		y;
+	int		fd;
+
+	y = -1;
+	x = -1;
+	line = NULL;
+	if((fd = open(path, O_RDONLY)) < 0)
+		return (-1);
+	while (++y >= 0 && get_next_line(fd, &line) > 0)
+	{
+		if (!(tab = ft_strsplit(line, ' ')))
+		{
+			ft_strdel(&line);
+			close(fd);
+			return (-1);
+		}
+		ft_strdel(&line);
+		while (tab[++x])
+			map->map[y][x] = ft_atoi(tab[x]);
+		x = -1;
+	}
+	close(fd);
+	return (0);
+}
+
+
+// DEBUG
+/*
+void	print_int_tab(int **map, int x, int y)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (i < y)
+	{
+		while (j < x)
+		{
+			ft_putnbr(map[i][j]);
+			ft_putchar(' ');
+			j++;
+		}
+		j = 0;
+		i++;
+		ft_putchar('\n');
+	}
+}*/
+
 int		fdf_parse(char *path, t_fmap *map)
 {
-	char ***temp;
-
-	temp = NULL;
 	if (fdf_verify(path, map) != 0)
 	{
 		ft_putstr_fd("fdf: invalid file.\n", 2);
 		return (-1);
 	}
-	if (!(temp = (char***)malloc(sizeof(char**) * (map->size_y + 1))))
-		return (-1);
-	if (!(map->map = gen_map(map->size_x, map->size_y)))
+	if (!(map->map = gen_map(map->size_x, map->size_y)) || fill_map(path, map, NULL) != 0)
 	{
-		free(temp);
+		ft_putstr_fd("fdf: error\n", 2);
+		delmap(&(map->map), map->size_y);
 		return (-1);
 	}
-	printf("OK\n");
 	return (0);
 }
